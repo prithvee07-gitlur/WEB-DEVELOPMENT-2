47bb8f51cb7b711565fca82277531348315cf57b
prithvee07-gitlur <prithveeyadav06@gmail.com>
2026-02-02 16:16:02 +0530

    session 8

---
+++ JS Day 8/script.js
@@ -0,0 +1,19 @@
+const heading = document.getElementById("myHeading")
+console. log (heading);
+
+heading.style.color="tomato";
+heading.style.backgroundColor="yellow";
+
+const para = document.getElementsByClassName
+("para");
+console. log (para);
+
+for(let i=0;i<para. length; i++){
+para[i].style.backgroundColor="green";
+}
+
+// queryselector
+
+const qs =document.querySelector("#content p")
+console. log (qs);
+qs.textContent="lorem djkabn. j d. eqjo. e j";
