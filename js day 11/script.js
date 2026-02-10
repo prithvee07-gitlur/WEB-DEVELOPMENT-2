84189a3000b08754e1417eaa6f4254a244c10364
prithvee07-gitlur <prithveeyadav06@gmail.com>
2026-02-10 15:18:07 +0530

    Added js day 11 folder

---
+++ js day 11/script.js
@@ -0,0 +1,15 @@
+// const textInput=document.getElementById("myInput");
+// textInput.addEventListener("change", (event)=>
+//     {
+// // here event is targeting the entire
+// // element object node and event.target is
+// // targetting the particular element.
+// event.preventDefault ()
+// console.log(event.target.id) ;
+
+// })
+
+const form=document.getElementById("formInput")
+const textInput=document.getElementById("myInput")
+const courseInput=document.getElementById("course")
+const output=document.getElementById("output")
